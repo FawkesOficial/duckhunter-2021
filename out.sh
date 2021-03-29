ab4e9d08bd4de2243f08efc9fe66fc0c20ee6ac0
Fawkes <45067011+FawkesOficial@users.noreply.github.com>
2021-03-29 22:02:40 +0100

    Update out.sh

--- out.sh
+++ out.sh
@@ -1,13 +1,13 @@
 #!/system/bin/sh
 
 sleep 3.0
-echo 'H --left-shift' | /system/xbin/hid-keyboard /dev/hidg0 keyboard
+echo 'h --left-shift' | /system/xbin/hid-keyboard /dev/hidg0 keyboard
 echo 'e' | /system/xbin/hid-keyboard /dev/hidg0 keyboard
 echo 'l' | /system/xbin/hid-keyboard /dev/hidg0 keyboard
 echo 'l' | /system/xbin/hid-keyboard /dev/hidg0 keyboard
 echo 'o' | /system/xbin/hid-keyboard /dev/hidg0 keyboard
 echo 'space' | /system/xbin/hid-keyboard /dev/hidg0 keyboard
-echo 'W --left-shift' | /system/xbin/hid-keyboard /dev/hidg0 keyboard
+echo 'h --left-shift' | /system/xbin/hid-keyboard /dev/hidg0 keyboard
 echo 'o' | /system/xbin/hid-keyboard /dev/hidg0 keyboard
 echo 'r' | /system/xbin/hid-keyboard /dev/hidg0 keyboard
 echo 'l' | /system/xbin/hid-keyboard /dev/hidg0 keyboard
